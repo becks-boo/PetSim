627fe7ea36dfe96cbe885518640e6e95308c80ef
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2022-05-05 17:19:26 +0200

    added dynamic change of hunger and happiness depending on playtime of device

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +15,7 @@ public class Robot : MonoBehaviour
 
     void Start()
     {
+        PlayerPrefs.SetString("then", "05/05/2022 11:20:12");
         UpdateStatus();
     }
 
@@ -38,13 +41,35 @@ public class Robot : MonoBehaviour
             _happiness = PlayerPrefs.GetInt("_happiness");
         }
 
+        if (!PlayerPrefs.HasKey("then"))
+        {
+            PlayerPrefs.SetString("then", GetStringTime());
+        }
+
+        TimeSpan ts = GetTimeSpan();
+
+        _hunger -= (int)(ts.TotalHours * 2);
+        if (_hunger < 0)
+        {
+            _hunger = 0;
+        }
+
+        _happiness -= (int)((100 - _hunger) * (ts.TotalHours / 5));
+        if (_happiness < 0)
+        {
+            _happiness = 0;
+        }
+
+        // Debug.Log(GetTimeSpan().ToString());
+        // Debug.Log(GetTimeSpan().TotalHours);
+
         if (_serverTime)
         {
             UpdateServer();
         }
         else
         {
-            UpdateDevice();
+            InvokeRepeating("UpdateDevice", 0f, 30f);
         }
     }
 
@@ -55,7 +80,27 @@ public class Robot : MonoBehaviour
 
     void UpdateDevice()
     {
+        PlayerPrefs.SetString("then", GetStringTime());
+    }
 
+    private TimeSpan GetTimeSpan()
+    {
+        CultureInfo myCultureInfo = new CultureInfo("de-DE");
+        if (_serverTime)
+        {
+            return new TimeSpan();
+        }
+        else
+        {
+            return DateTime.Now - Convert.ToDateTime(PlayerPrefs.GetString("then"), myCultureInfo);
+        }
+    }
+
+    private string GetStringTime()
+    {
+        DateTime now = DateTime.Now;
+        return now.Month + "/" + now.Day + "/" + now.Year + " " + now.Hour + ":"
+            + now.Minute + ":" + now.Second;
     }
 
     public int hunger
@@ -69,5 +114,4 @@ public class Robot : MonoBehaviour
         get { return _happiness; }
         set { _happiness = value; }
     }
-    
 }
